8535ea12ebdf8c411764b2917a8c632f9f793598
miusuariodegit <eligevivirenpaz@hotmail.com>
2025-12-07 15:53:30 -0600

    ejercicio6

---
+++ ejercicio6/aplicacion/conexion/conexionBD.py
@@ -0,0 +1,25 @@
+
+
+# Importando Libreria mysql.connector para conectar Python con MySQL
+import mysql.connector
+
+
+def connectionBD():
+    try:
+        # connection = mysql.connector.connect(
+        connection = mysql.connector.connect(
+            host="db",
+            user="usuario",
+            passwd="mipassword",
+            database="crud_python",
+            charset='utf8mb4',
+            collation='utf8mb4_unicode_ci',
+            raise_on_warnings=True
+
+        )
+        if connection.is_connected():
+            # print("Conexión exitosa a la BD")
+            return connection
+
+    except mysql.connector.Error as error:
+        print(f"No se pudo conectar: {error}")
