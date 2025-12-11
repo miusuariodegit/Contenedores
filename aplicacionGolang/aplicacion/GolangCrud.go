555a59e42217b6b3147414012747c89aa4d673c6
miusuariodegit <eligevivirenpaz@hotmail.com>
2025-12-11 11:14:43 -0600

    golang

--- aplicacionGolang/aplicacion/GolangCrud.go
+++ aplicacionGolang/aplicacion/GolangCrud.go
@@ -2,9 +2,21 @@ package main
 
 
 import (
+    "database/sql"
+    "encoding/json"
     "fmt"
+    "log"
     "net/http"
+    "strconv"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gorilla/mux"
 )
+
+type User struct {
+  ID    int
+  Name  string
+  Email string
+}
 
 func hello(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "hello\n")
@@ -19,11 +31,186 @@ func headers(w http.ResponseWriter, req *http.Request) {
     }
 }
 
-	
-
 func main() {
 
+    http.HandleFunc("/user", createUserHandler).Methods("POST")
+    http.HandleFunc("/user/{id}", getUserHandler).Methods("GET")
+    http.HandleFunc("/user/{id}", updateUserHandler).Methods("PUT")
+    http.HandleFunc("/user/{id}", deleteUserHandler).Methods("DELETE")
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/headers", headers)
+	log.Println("Server listening on :8090")
     http.ListenAndServe(":8090", nil)
 }
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+    dsn := "usuario:mipassword@tcp(localhost:3306)/gocrud_app"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+      panic(err.Error())
+    }
+    defer db.Close()
+
+    // Parse JSON data from the request body
+    var user User
+    json.NewDecoder(r.Body).Decode(&user)
+
+    CreateUser(db, user.Name, user.Email)
+    if err != nil {
+     http.Error(w, "Failed to create user", http.StatusInternalServerError)
+     return
+    }
+
+    w.WriteHeader(http.StatusCreated)
+    fmt.Fprintln(w, "User created successfully")
+}
+
+
+
+func CreateUser(db *sql.DB, name, email string) error {
+    query := "INSERT INTO users (name, email) VALUES (?, ?)"
+    _, err := db.Exec(query, name, email)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+
+
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+    dsn := "usuario:mipassword@tcp(localhost:3306)/gocrud_app"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+      panic(err.Error())
+    }
+    defer db.Close()
+
+    // Get the 'id' parameter from the URL
+    vars := mux.Vars(r)
+    idStr := vars["id"]
+
+    // Convert 'id' to an integer
+    userID, err := strconv.Atoi(idStr)
+
+    // Call the GetUser function to fetch the user data from the database
+    user, err := GetUser(db, userID)
+    if err != nil {
+      http.Error(w, "User not found", http.StatusNotFound)
+      return
+    }
+
+    // Convert the user object to JSON and send it in the response
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(user)
+ }
+
+
+
+func GetUser(db *sql.DB, id int) (*User, error) {
+    query := "SELECT * FROM users WHERE id = ?"
+    row := db.QueryRow(query, id)
+
+    user := &User{}
+    err := row.Scan(&user.ID, &user.Name, &user.Email)
+    if err != nil {
+        return nil, err
+    }
+    return user, nil
+}
+
+
+
+func UpdateUser(db *sql.DB, id int, name, email string) error {
+    query := "UPDATE users SET name = ?, email = ? WHERE id = ?"
+    _, err := db.Exec(query, name, email, id)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+
+
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+    dsn := "usuario:mipassword@tcp(localhost:3306)/gocrud_app"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+      panic(err.Error())
+    }
+    defer db.Close()
+
+    // Get the 'id' parameter from the URL
+    vars := mux.Vars(r)
+    idStr := vars["id"]
+
+    // Convert 'id' to an integer
+    userID, err := strconv.Atoi(idStr)
+
+    var user User
+    err = json.NewDecoder(r.Body).Decode(&user)
+
+    // Call the GetUser function to fetch the user data from the database
+    UpdateUser(db, userID, user.Name, user.Email)
+    if err != nil {
+      http.Error(w, "User not found", http.StatusNotFound)
+      return
+    }
+
+    fmt.Fprintln(w, "User updated successfully")
+}
+
+
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+    dsn := "usuario:mipassword@tcp(localhost:3306)/gocrud_app"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+      panic(err.Error())
+    }
+
+    defer db.Close()
+
+    // Get the 'id' parameter from the URL
+    vars := mux.Vars(r)
+    idStr := vars["id"]
+
+    // Convert 'id' to an integer
+    userID, err := strconv.Atoi(idStr)
+    if err != nil {
+      http.Error(w, "Invalid 'id' parameter", http.StatusBadRequest)
+      return
+    }
+
+    DeleteUser(db, userID)
+    if err != nil {
+      http.Error(w, "User not found", http.StatusNotFound)
+      return
+    }
+
+    fmt.Fprintln(w, "Usuario borrado satisfactoriamente, el objeto regresado es:")
+
+    // Convert the user object to JSON and send it in the response
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode("")
+}
+
+
+
+
+
+func DeleteUser(db *sql.DB, id int) error {
+    query := "DELETE FROM users WHERE id = ?"
+    _, err := db.Exec(query, id)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+
+
+
+
+
