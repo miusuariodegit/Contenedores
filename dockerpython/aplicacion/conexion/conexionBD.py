7be1e01fe2e82595adc345d15287c8bf0b0e734a
miusuariodegit <eligevivirenpaz@hotmail.com>
2025-12-13 13:14:57 -0600

    conexion python

--- dockerpython/aplicacion/conexion/conexionBD.py
+++ dockerpython/aplicacion/conexion/conexionBD.py
@@ -1,22 +1,29 @@
-
-
 # Importando Libreria mysql.connector para conectar Python con MySQL
 import mysql.connector
+from mysql.connector import Error
 
+config = {
+    'user': 'usuario',
+    'password': 'mipassword',
+    'host': 'mysql_db_python',  
+    'port': 3308,         
+    'database': 'crud_python',
+}
 
 def connectionBD():
     try:
         # connection = mysql.connector.connect(
-        connection = mysql.connector.connect(
-            host="mysql_db_python",
-            user="usuario",
-            passwd="mipassword",
-            database="crud_python",
-            charset='utf8mb4',
-            collation='utf8mb4_unicode_ci',
-            raise_on_warnings=True
-
-        )
+        connection = mysql.connector.connect(**config)
+        #connection = mysql.connector.connect(
+        #    host="mysql_db_python",
+        #    user="usuario",
+        #    passwd="mipassword",
+        #    database="crud_python",
+        #    charset='utf8mb4',
+        #    collation='utf8mb4_unicode_ci',
+        #    raise_on_warnings=True
+        #)
+        
         if connection.is_connected():
             # print("Conexión exitosa a la BD")
             return connection
