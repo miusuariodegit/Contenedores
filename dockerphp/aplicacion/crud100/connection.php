b2723a4809852028bf9dd169fbf608591a133c3d
miusuariodegit <eligevivirenpaz@hotmail.com>
2025-12-13 12:46:48 -0600

    php 3308

--- dockerphp/aplicacion/crud100/connection.php
+++ dockerphp/aplicacion/crud100/connection.php
@@ -3,7 +3,7 @@
     $username = "usuario";
     $password = "mipassword";
     $db_name = "crud";  
-    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
+    $conn = new mysqli($servername, $username, $password, $db_name, 3308);
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
     }
